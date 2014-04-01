0c7b0844da8b799a5c74b12b0d5e6e970d828d96
blk_jack <jcorcoran+github@gmail.com>
2014-04-01 12:29:02 -0400

    Strip down StatsWidgetSettings Activity

--- Apphangar/src/main/java/ca/mimic/apphangar/StatsWidgetSettings.java
+++ Apphangar/src/main/java/ca/mimic/apphangar/StatsWidgetSettings.java
@@ -1,38 +1,25 @@
 package ca.mimic.apphangar;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.preference.PreferenceActivity;
 
+import ca.mimic.apphangar.Settings.PrefsGet;
 
-public class StatsWidgetSettings extends Activity {
+public class StatsWidgetSettings extends PreferenceActivity {
+
+    static PrefsGet prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefs = new PrefsGet(getSharedPreferences(getPackageName(), Context.MODE_PRIVATE));
+        // addPreferencesFromResource(R.layout.stats_widget_settings);
         setContentView(R.layout.stats_widget_settings);
     }
-
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.stats_widget_settings, menu);
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
