ae78037d3692e27bec5a79fd896f6a84b9641f78
blk_jack <jcorcoran+github@gmail.com>
2014-04-12 03:57:44 -0400

    Fix widget settings. Fix manual app #.

--- Apphangar/src/main/java/ca/mimic/apphangar/AppsWidget.java
+++ Apphangar/src/main/java/ca/mimic/apphangar/AppsWidget.java
@@ -31,7 +31,6 @@ public class AppsWidget extends AppWidgetProvider {
     protected static TasksDataSource db;
     protected static Context mContext;
     protected static PrefsGet prefs;
-    protected static PrefsGet prefsSettings;
 
     protected static final String BCAST_CONFIGCHANGED = "android.intent.action.CONFIGURATION_CHANGED";
     protected static final int MAX_DB_LOOKUPS = 12;
@@ -76,18 +75,17 @@ public class AppsWidget extends AppWidgetProvider {
             int appWidgetId, Bundle options) {
 
         Log.d("Apphangar", "updateAppWidget");
-        prefs = new PrefsGet(context.getSharedPreferences("StatsWidget", Context.MODE_PRIVATE));
-        prefsSettings = new PrefsGet(context.getSharedPreferences(context.getPackageName(), Context.MODE_MULTI_PROCESS));
+        prefs = new PrefsGet(context.getSharedPreferences("AppsWidget", Context.MODE_PRIVATE));
 
         SharedPreferences mPrefs = prefs.prefsGet();
-        SharedPreferences mPrefsSettings = prefsSettings.prefsGet();
 
         int itemHeight = 55;
         int itemWidth = 55;
 
-        // boolean appsNoByWidgetSize = mPrefs.getBoolean(Settings.APPS_BY_WIDGET_SIZE_PREFERENCE, Settings.APPS_BY_WIDGET_SIZE_DEFAULT);
+        boolean appsNoByWidgetSize = mPrefs.getBoolean(Settings.APPS_BY_WIDGET_SIZE_PREFERENCE, Settings.APPS_BY_WIDGET_SIZE_DEFAULT);
         int appsNoH;
         int appsNoW;
+        boolean autoHeight = true;
 
         Log.d(Settings.TAG, "jeff minHeight: " + options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT));
         Log.d(Settings.TAG, "jeff maxHeight: " + options.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT));
@@ -97,25 +95,34 @@ public class AppsWidget extends AppWidgetProvider {
         appsNoW = (int) Math.floor((options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH) - 10) / itemWidth);
         Log.d(Settings.TAG, "jeff appsNoH: "  + appsNoH + " appsNoW: " + appsNoW);
 
-//        if (appsNoByWidgetSize && appsNo > 0) {
-//            Log.d("Apphangar", "appsNoByWidgetSize=true, appsNo=" + appsNo);
-//        } else {
-//            appsNo = Integer.parseInt(mPrefs.getString(Settings.STATS_WIDGET_APPSNO_PREFERENCE, Integer.toString(Settings.STATS_WIDGET_APPSNO_DEFAULT)));
-//        }
+        if (appsNoH == 0) {
+            appsNoH = 1;
+            autoHeight = false;
+        }
 
         if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
             appsNoH = (int) Math.floor((options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT) - 10) / itemHeight);
+            if (appsNoH == 0) {
+                appsNoH = 1;
+                autoHeight = false;
+            }
             appsNoW = (int) Math.floor((options.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH) - 10) / itemWidth);
-//            if (appsNoByWidgetSize && appsNoW > 0) {
-//                appsNo = appsNoLs;
-//                Log.d("Apphangar", "Landscape! appsNoByWidgetSize=true, appsNo=" + appsNo);
-//            } else {
-//                appsNo = Integer.parseInt(mPrefs.getString(Settings.STATS_WIDGET_APPSNO_LS_PREFERENCE, Integer.toString(Settings.STATS_WIDGET_APPSNO_LS_DEFAULT)));
-//            }
+            if (appsNoByWidgetSize && appsNoW > 0) {
+                Log.d("Apphangar", "Landscape! appsNoByWidgetSize=true, appsNo=" + appsNoW);
+            } else {
+                appsNoW = Integer.parseInt(mPrefs.getString(Settings.STATS_WIDGET_APPSNO_LS_PREFERENCE, Integer.toString(Settings.STATS_WIDGET_APPSNO_LS_DEFAULT)));
+            }
             Log.d("Apphangar", "LANDSCAPE");
+        } else {
+            if (appsNoByWidgetSize && appsNoW > 0) {
+                Log.d("Apphangar", "appsNoByWidgetSize=true, appsNo=" + appsNoH);
+            } else {
+                appsNoW = Integer.parseInt(mPrefs.getString(Settings.STATS_WIDGET_APPSNO_PREFERENCE, Integer.toString(Settings.STATS_WIDGET_APPSNO_DEFAULT)));
+            }
         }
 
-        int getColor = mPrefs.getInt(Settings.BACKGROUND_COLOR_PREFERENCE, Settings.BACKGROUND_COLOR_DEFAULT);
+        int getBackgroundColor = mPrefs.getInt(Settings.BACKGROUND_COLOR_PREFERENCE, Settings.BACKGROUND_COLOR_DEFAULT);
+        int getColor = mPrefs.getInt(Settings.ICON_COLOR_PREFERENCE, Settings.ICON_COLOR_DEFAULT);
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.apps_widget);
         views.removeAllViews(R.id.viewCont);
@@ -128,7 +135,14 @@ public class AppsWidget extends AppWidgetProvider {
 
         ArrayList<Tools.TaskInfo> appList = new ArrayList<Tools.TaskInfo>();
 
-        int lookUpNum = (appsNoH * appsNoW) + 3; // 3 = saftey buffer
+        int origWidth = (appsNoH * appsNoW);
+        int lookUpNum = origWidth + 3; // 3 = saftey buffer
+
+        if (autoHeight && !appsNoByWidgetSize) {
+            // Manual app # is selected.  Icons are split automatically from height.
+            origWidth = appsNoW;
+            lookUpNum = appsNoW + 3;
+        }
         List<TasksModel> tasks = db.getAllTasks((lookUpNum < MAX_DB_LOOKUPS) ? MAX_DB_LOOKUPS : lookUpNum);
 
         for (TasksModel taskM : tasks) {
@@ -155,9 +169,10 @@ public class AppsWidget extends AppWidgetProvider {
 
         String taskPackage = context.getPackageName();
 
-        boolean weightedRecents = mPrefsSettings.getBoolean(Settings.WEIGHTED_RECENTS_PREFERENCE,
+        boolean isColorized = mPrefs.getBoolean(Settings.COLORIZE_PREFERENCE, Settings.COLORIZE_DEFAULT);
+        boolean weightedRecents = mPrefs.getBoolean(Settings.WEIGHTED_RECENTS_PREFERENCE,
                 Settings.WEIGHTED_RECENTS_DEFAULT);
-        int weightPriority = Integer.parseInt(mPrefsSettings.getString(Settings.WEIGHT_PRIORITY_PREFERENCE,
+        int weightPriority = Integer.parseInt(mPrefs.getString(Settings.WEIGHT_PRIORITY_PREFERENCE,
                 Integer.toString(Settings.WEIGHT_PRIORITY_DEFAULT)));
         if (weightedRecents) {
             Log.d(Settings.TAG, " wP: " + weightPriority);
@@ -165,19 +180,28 @@ public class AppsWidget extends AppWidgetProvider {
         }
 
         RemoteViews row = new RemoteViews(context.getPackageName(), R.layout.apps_widget_row);
-        row.setInt(R.id.viewRow, "setBackgroundColor", getColor);
+        row.setInt(R.id.viewRow, "setBackgroundColor", getBackgroundColor);
 
+        if (autoHeight && !appsNoByWidgetSize) {
+            appsNoW = (int) Math.ceil((double) appsNoW / appsNoH);
+            Log.d(Settings.TAG, "autoHeight true (start), appsNoW=" + appsNoW);
+        }
         int filledConts = 0;
         int filledRows = 1;
         for (int i=0; i < appList.size(); i++) {
 
             if (filledConts == appsNoW || i == (appList.size()-1)) {
-                filledConts = 0;
                 views.addView(R.id.viewCont, row);
-                if (filledRows < appsNoH) {
-                    filledRows++;
+                if (filledRows < appsNoH && filledConts < origWidth) {
                     row = new RemoteViews(context.getPackageName(), R.layout.apps_widget_row);
-                    row.setInt(R.id.viewRow, "setBackgroundColor", getColor);
+                    row.setInt(R.id.viewRow, "setBackgroundColor", getBackgroundColor);
+//                    if (autoHeight && !appsNoByWidgetSize) {
+//                        leftoverWidth -= appsNoW;
+//                        appsNoW = (int) Math.ceil((double) leftoverWidth / (appsNoH - filledRows));
+//                        Log.d(Settings.TAG, "autoHeight true, appsNoW=" + appsNoW);
+//                    }
+                    filledConts = 0;
+                    filledRows++;
                 } else {
                     // Log.d(TAG, "filledConts [" + filledConts + "] == maxButtons [" + maxButtons + "]");
                     break;
@@ -186,8 +210,13 @@ public class AppsWidget extends AppWidgetProvider {
 
             filledConts += 1;
 
-            int resID = context.getResources().getIdentifier("imageButton" + (filledConts+1), "id", taskPackage);
-            int contID = context.getResources().getIdentifier("imageCont" + (filledConts+1), "id", taskPackage);
+            int resID = context.getResources().getIdentifier("imageButton" + (filledConts), "id", taskPackage);
+            int contID = context.getResources().getIdentifier("imageCont" + (filledConts), "id", taskPackage);
+
+            if (i >= origWidth) {
+                row.setViewVisibility(contID, View.INVISIBLE);
+                continue;
+            }
 
             row.setViewVisibility(contID, View.VISIBLE);
             Log.d(Settings.TAG, "Setting cont visible: " + filledConts + " [" + appList.get(i).appName + "]");
@@ -202,11 +231,11 @@ public class AppsWidget extends AppWidgetProvider {
                 continue;
             }
 
-//            if (isColorized) {
-//                d = new BitmapDrawable(ColorHelper.getColoredBitmap(taskIcon, getColor));
-//            } else {
-              d = taskIcon;
-//            }
+            if (isColorized) {
+                d = new BitmapDrawable(ColorHelper.getColoredBitmap(taskIcon, getColor));
+            } else {
+                d = taskIcon;
+            }
 
             Bitmap bmpIcon = ((BitmapDrawable) d).getBitmap();
             row.setImageViewBitmap(resID, bmpIcon);
